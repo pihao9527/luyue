76a708e1557f6b38947c46d620eaa8ed9a152323
zsmj <391360894@qq.com>
2018-09-09 21:46:37 +0800

    zuul 配置

--- luyue_user/src/main/java/com/example/luyue_user/redis/RedisAction.java
+++ luyue_user/src/main/java/com/example/luyue_user/redis/RedisAction.java
@@ -1,19 +1,19 @@
-package com.example.luyue_user.redis;
-
-import com.example.luyue_user_api.api.UserServiceApi;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/user")
-public class RedisAction implements UserServiceApi {
-    @Autowired
-    RedisDao redisDao;
-
-    @Override
-    public boolean setUserName(@RequestBody String userName) {
-        return redisDao.setKey(userName,userName);
-    }
-}
+//package com.example.luyue_user.redis;
+//
+//import com.example.luyue_user_api.api.UserServiceApi;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/user")
+//public class RedisAction implements UserServiceApi {
+//    @Autowired
+//    RedisDao redisDao;
+//
+//    @Override
+//    public boolean setUserName(@RequestBody String userName) {
+//        return redisDao.setKey(userName,userName);
+//    }
+//}
--- luyue_user/src/main/java/com/example/luyue_user/redis/redisDao.java
+++ luyue_user/src/main/java/com/example/luyue_user/redis/redisDao.java
@@ -1,27 +1,27 @@
-package com.example.luyue_user.redis;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.stereotype.Repository;
-
-import java.util.concurrent.TimeUnit;
-
-@Repository
-class RedisDao {
-
-    @Autowired
-    private StringRedisTemplate template;
-
-    public  boolean setKey(String key,String value){
-        ValueOperations<String, String> ops = template.opsForValue();
-        ops.set(key,value,1, TimeUnit.MINUTES);//1分钟过期
-        return true;
-    }
-
-    public String getValue(String key){
-        ValueOperations<String, String> ops = this.template.opsForValue();
-        return ops.get(key);
-    }
-}
-
+//package com.example.luyue_user.redis;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.data.redis.core.ValueOperations;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//@Repository
+//class RedisDao {
+//
+//    @Autowired
+//    private StringRedisTemplate template;
+//
+//    public  boolean setKey(String key,String value){
+//        ValueOperations<String, String> ops = template.opsForValue();
+//        ops.set(key,value,1, TimeUnit.MINUTES);//1分钟过期
+//        return true;
+//    }
+//
+//    public String getValue(String key){
+//        ValueOperations<String, String> ops = this.template.opsForValue();
+//        return ops.get(key);
+//    }
+//}
+//
