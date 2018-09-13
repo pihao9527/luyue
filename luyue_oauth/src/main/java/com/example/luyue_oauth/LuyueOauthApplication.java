1664580adab7b1d4d0358ddb52507b441135b1d9
zsmj <391360894@qq.com>
2018-09-13 22:39:06 +0800

    增加oauth2

---
+++ luyue_oauth/src/main/java/com/example/luyue_oauth/LuyueOauthApplication.java
@@ -0,0 +1,12 @@
+package com.example.luyue_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuyueOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuyueOauthApplication.class, args);
+	}
+}
